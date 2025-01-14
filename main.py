4d5a3d89c82abad36a3bb8c399e6d08c593cfa55
EmmaTheMartian <emmathemartian@gmail.com>
2025-01-13 21:04:15 -0600

    hopefully get bot working

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import flask
+from flask import Flask, request
 import dotenv
 import git
 import hmac
 import hashlib
 import os
+import requests
+import requests.cookies
+import datetime
 
 dotenv.load_dotenv()
 
@@ -11,9 +14,12 @@ BOT_USERNAME = os.environ['BOT_USERNAME']
 BOT_PASSWORD = os.environ['BOT_PASSWORD']
 RELOAD_REPO_SECRET = os.environ['RELOAD_REPO_SECRET']
 BEEP_WEBHOOK_SECRET = os.environ['BEEP_WEBHOOK_SECRET']
+BEEP_URL = os.environ['BEEP_URL']
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 repo = git.Repo('./')
+session = requests.Session()
+
 
 def is_valid_signature(x_hub_signature, data, private_key) -> bool:
 	# x_hub_signature and data are from the webhook payload
@@ -26,25 +32,68 @@ def is_valid_signature(x_hub_signature, data, private_key) -> bool:
 
 @app.route('/webhook', methods = ['POST'])
 def webhook():
-	print(flask.request.json)
-	return flask.Response(status = 200)
+	x_hub_signature = request.headers.get('X-Hub-Signature')
+	if not is_valid_signature(x_hub_signature, request.data, RELOAD_REPO_SECRET):
+		return 'invalid signature', 418
+
+	if request.method != 'POST':
+		return 'wrong event type', 400
+
+	if session.cookies.get('token', default = '') == '':
+		return 'bot not logged in'
+
+	data = request.get_json()
+	commits = data['commits']
+	body = f'{data['before']} -> {data['after']}\n---\n'
+	body += '\n'.join([f'- {it['message']} ({it['url']})' for it in commits])
+	session.post(BEEP_URL + '/api/post/new_post', timeout = 10, data = {
+		'title': f'pushed {len(commits)} commit{'s' if len(commits) > 1 else ''}',
+		'body': body
+	})
+
+	return 'updated successfully'
+
 
 @app.route('/update', methods = ['POST'])
 def update():
-	x_hub_signature = flask.request.headers.get('X-Hub-Signature')
-	if not is_valid_signature(x_hub_signature, flask.request.data, RELOAD_REPO_SECRET):
+	x_hub_signature = request.headers.get('X-Hub-Signature')
+	if not is_valid_signature(x_hub_signature, request.data, RELOAD_REPO_SECRET):
 		return 'invalid signature', 418
 
-	if flask.request.method == 'POST':
-		print('-> updating bot...')
-		repo.remotes.origin.pull()
-		return 'updated successfully'
-	else:
+	if request.method != 'POST':
 		return 'wrong event type', 400
+
+	print('-> updating bot...')
+	repo.remotes.origin.pull()
+	return 'updated successfully'
 
 @app.route('/')
 def index():
-	return 'webapp running :sunglasses: (commit: ' + repo.commit().hexsha + ')'
+	return 'bot running :sunglasses: (commit: ' + repo.commit().hexsha + ')'
 
 if __name__ == '__main__':
+	start = datetime.datetime.now()
+
+	# log into beep
+	print('-> logging in...')
+	response = session.post(BEEP_URL + '/api/user/login', data = {
+		'username': BOT_USERNAME,
+		'password': BOT_PASSWORD
+	})
+
+	whoami = session.get(BEEP_URL + '/api/user/whoami')
+	print('-> logged in as: ' + whoami.text)
+
+	stop = datetime.datetime.now() - start
+
+	# make a test post
+	session.post(BEEP_URL + '/api/post/new_post', data = {
+		'title': 'bot online',
+		'body': f'took {stop.seconds}s to start.'
+	})
+
+	# run app
 	app.run()
+
+	print('-> logging out...')
+	requests.post(BEEP_URL + '/api/user/full_logout')
